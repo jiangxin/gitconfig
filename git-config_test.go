jiangxin/goconfig

Add GitConfig to read boolean, int, multi-values
Some of git configs have multiple values, such as: `push.pushOption`,
`include.path`, and `remote.<name>.fetch`.

Implement new struct `GitConfig` in `gitconfig.go` to store value(s) in
array to support multiple values.

Signed-off-by: Jiang Xin <worldhello.net@gmail.com>

git-config_test.go
@@ -1,0 +1,161 @@
+package goconfig
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInvalidSectionName(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `# The following section name should have quote, like: [a "b"]
+[a b]
+	c = d`
+	_, lineno, err := Parse([]byte(data))
+	assert.Equal(ErrMissingStartQuote, err)
+	assert.Equal(uint(2), lineno)
+}
+
+func TestInvalidKeyWithSpace(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `# keys should not have spaces
+[a]
+	b c = d`
+	_, lineno, err := Parse([]byte(data))
+	assert.Equal(ErrInvalidKeyChar, err)
+	assert.Equal(uint(3), lineno)
+}
+
+func TestParseSectionWithSpaces1(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[ab "cd"]
+	value1 = x
+	value2 = x y
+	value3  = a \"quote
+[remote "hello world"]
+	url = test`
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+	assert.Equal("x", cfg.Get("ab.cd.value1"))
+	assert.Equal("x y", cfg.Get("ab.cd.value2"))
+	assert.Equal("a \"quote", cfg.Get("ab.cd.value3"))
+}
+
+func TestParseSectionWithSpaces2(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[remote "hello world"]
+	url = test`
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+	assert.Equal("test", cfg.Get("remote.hello world.url"))
+	assert.Equal("test", cfg.Get(`remote."hello world".url`))
+	assert.Equal("test", cfg.Get(`"remote.hello world".url`))
+	assert.Equal("test", cfg.Get(`"remote.hello world.url"`))
+}
+
+func TestGetAll(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[remote "origin"]
+	url = https://example.com/my/repo.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+	fetch = +refs/tags/*:refs/tags/*`
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+	assert.Equal("+refs/tags/*:refs/tags/*", cfg.Get("remote.origin.fetch"))
+	assert.Equal([]string{
+		"+refs/heads/*:refs/remotes/origin/*",
+		"+refs/tags/*:refs/tags/*",
+	}, cfg.GetAll("remote.origin.fetch"))
+
+}
+
+func TestGetBool(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[a]
+	t1 = true
+	t2 = yes
+	t3 = on
+	f1 = false
+	f2 = no
+	f3 = off
+	x1 = 1
+	x2 = nothing`
+
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+
+	v, err := cfg.GetBool("a.t1", false)
+	assert.Nil(err)
+	assert.True(v)
+
+	v, err = cfg.GetBool("a.t2", false)
+	assert.Nil(err)
+	assert.True(v)
+
+	v, err = cfg.GetBool("a.t3", false)
+	assert.Nil(err)
+	assert.True(v)
+
+	v, err = cfg.GetBool("a.t4", false)
+	assert.Nil(err)
+	assert.False(v)
+
+	v, err = cfg.GetBool("a.f1", true)
+	assert.Nil(err)
+	assert.False(v)
+
+	v, err = cfg.GetBool("a.f2", true)
+	assert.Nil(err)
+	assert.False(v)
+
+	v, err = cfg.GetBool("a.f3", true)
+	assert.Nil(err)
+	assert.False(v)
+
+	v, err = cfg.GetBool("a.f4", true)
+	assert.Nil(err)
+	assert.True(v)
+
+	v, err = cfg.GetBool("a.x1", true)
+	assert.Equal(ErrNotBoolValue, err)
+
+	v, err = cfg.GetBool("a.x2", true)
+	assert.Equal(ErrNotBoolValue, err)
+}
+
+func TestGetInt(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[a]
+	i1 = 1
+	i2 = 100
+	i3 = abc`
+
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+
+	v1, err := cfg.GetInt("a.i1", 0)
+	assert.Nil(err)
+	assert.Equal(1, v1)
+
+	v2, err := cfg.GetInt64("a.i2", 0)
+	assert.Nil(err)
+	assert.Equal(int64(100), v2)
+
+	v3, err := cfg.GetUint64("a.i2", 0)
+	assert.Nil(err)
+	assert.Equal(uint64(100), v3)
+
+	_, err = cfg.GetInt("a.i3", 0)
+	assert.NotNil(err)
+
+	v4, err := cfg.GetInt("a.i4", 6700)
+	assert.Nil(err)
+	assert.Equal(6700, v4)
+}

jiangxin/gitconfig

Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

git-config_test.go
@@ -1,0 +1,161 @@
+package gitconfig
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInvalidSectionName(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `# The following section name should have quote, like: [a "b"]
+[a b]
+	c = d`
+	_, lineno, err := Parse([]byte(data))
+	assert.Equal(ErrMissingStartQuote, err)
+	assert.Equal(uint(2), lineno)
+}
+
+func TestInvalidKeyWithSpace(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `# keys should not have spaces
+[a]
+	b c = d`
+	_, lineno, err := Parse([]byte(data))
+	assert.Equal(ErrInvalidKeyChar, err)
+	assert.Equal(uint(3), lineno)
+}
+
+func TestParseSectionWithSpaces1(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[ab "cd"]
+	value1 = x
+	value2 = x y
+	value3  = a \"quote
+[remote "hello world"]
+	url = test`
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+	assert.Equal("x", cfg.Get("ab.cd.value1"))
+	assert.Equal("x y", cfg.Get("ab.cd.value2"))
+	assert.Equal("a \"quote", cfg.Get("ab.cd.value3"))
+}
+
+func TestParseSectionWithSpaces2(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[remote "hello world"]
+	url = test`
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+	assert.Equal("test", cfg.Get("remote.hello world.url"))
+	assert.Equal("test", cfg.Get(`remote."hello world".url`))
+	assert.Equal("test", cfg.Get(`"remote.hello world".url`))
+	assert.Equal("test", cfg.Get(`"remote.hello world.url"`))
+}
+
+func TestGetAll(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[remote "origin"]
+	url = https://example.com/my/repo.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+	fetch = +refs/tags/*:refs/tags/*`
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+	assert.Equal("+refs/tags/*:refs/tags/*", cfg.Get("remote.origin.fetch"))
+	assert.Equal([]string{
+		"+refs/heads/*:refs/remotes/origin/*",
+		"+refs/tags/*:refs/tags/*",
+	}, cfg.GetAll("remote.origin.fetch"))
+
+}
+
+func TestGetBool(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[a]
+	t1 = true
+	t2 = yes
+	t3 = on
+	f1 = false
+	f2 = no
+	f3 = off
+	x1 = 1
+	x2 = nothing`
+
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+
+	v, err := cfg.GetBool("a.t1", false)
+	assert.Nil(err)
+	assert.True(v)
+
+	v, err = cfg.GetBool("a.t2", false)
+	assert.Nil(err)
+	assert.True(v)
+
+	v, err = cfg.GetBool("a.t3", false)
+	assert.Nil(err)
+	assert.True(v)
+
+	v, err = cfg.GetBool("a.t4", false)
+	assert.Nil(err)
+	assert.False(v)
+
+	v, err = cfg.GetBool("a.f1", true)
+	assert.Nil(err)
+	assert.False(v)
+
+	v, err = cfg.GetBool("a.f2", true)
+	assert.Nil(err)
+	assert.False(v)
+
+	v, err = cfg.GetBool("a.f3", true)
+	assert.Nil(err)
+	assert.False(v)
+
+	v, err = cfg.GetBool("a.f4", true)
+	assert.Nil(err)
+	assert.True(v)
+
+	v, err = cfg.GetBool("a.x1", true)
+	assert.Equal(ErrNotBoolValue, err)
+
+	v, err = cfg.GetBool("a.x2", true)
+	assert.Equal(ErrNotBoolValue, err)
+}
+
+func TestGetInt(t *testing.T) {
+	assert := assert.New(t)
+
+	data := `[a]
+	i1 = 1
+	i2 = 100
+	i3 = abc`
+
+	cfg, _, err := Parse([]byte(data))
+	assert.Nil(err)
+
+	v1, err := cfg.GetInt("a.i1", 0)
+	assert.Nil(err)
+	assert.Equal(1, v1)
+
+	v2, err := cfg.GetInt64("a.i2", 0)
+	assert.Nil(err)
+	assert.Equal(int64(100), v2)
+
+	v3, err := cfg.GetUint64("a.i2", 0)
+	assert.Nil(err)
+	assert.Equal(uint64(100), v3)
+
+	_, err = cfg.GetInt("a.i3", 0)
+	assert.NotNil(err)
+
+	v4, err := cfg.GetInt("a.i4", 6700)
+	assert.Nil(err)
+	assert.Equal(6700, v4)
+}