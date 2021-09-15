jiangxin/goconfig

Parse include config files from include.path
One git config file can include other config files by `include.path`
directions. Parse recursive if there is an `include.path` direction.

Note: not support contional includes yet.

Signed-off-by: Jiang Xin <worldhello.net@gmail.com>

path.go
@@ -1,0 +1,75 @@
+package goconfig
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func homeDir() (string, error) {
+	var (
+		home string
+	)
+
+	if runtime.GOOS == "windows" {
+		home = os.Getenv("USERPROFILE")
+		if home == "" {
+			home = os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		}
+	}
+	if home == "" {
+		home = os.Getenv("HOME")
+	}
+
+	if home == "" {
+		return "", fmt.Errorf("cannot find HOME")
+	}
+
+	return home, nil
+}
+
+func expendHome(name string) (string, error) {
+	if filepath.IsAbs(name) {
+		return name, nil
+	}
+
+	home, err := homeDir()
+	if err != nil {
+		return "", err
+	}
+
+	if len(name) == 0 || name == "~" {
+		return home, nil
+	} else if len(name) > 1 && name[0] == '~' && (name[1] == '/' || name[1] == '\\') {
+		return filepath.Join(home, name[2:]), nil
+	}
+
+	return filepath.Join(home, name), nil
+}
+
+// Abs returns absolute path and will expend homedir if path has "~/' prefix
+func Abs(name string) (string, error) {
+	if filepath.IsAbs(name) {
+		return name, nil
+	}
+
+	if len(name) > 0 && name[0] == '~' && (len(name) == 1 || name[1] == '/' || name[1] == '\\') {
+		return expendHome(name)
+	}
+
+	return filepath.Abs(name)
+}
+
+// AbsJoin returns absolute path, and use <dir> as parent dir for relative path
+func AbsJoin(dir, name string) (string, error) {
+	if filepath.IsAbs(name) {
+		return name, nil
+	}
+
+	if len(name) > 0 && name[0] == '~' && (len(name) == 1 || name[1] == '/' || name[1] == '\\') {
+		return expendHome(name)
+	}
+
+	return Abs(filepath.Join(dir, name))
+}

path_test.go
@@ -1,0 +1,209 @@
+package goconfig
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExpendHome(t *testing.T) {
+	var (
+		home   string
+		tmpdir string
+		name   string
+		err    error
+		assert = assert.New(t)
+	)
+
+	tmpdir, err = ioutil.TempDir("", "goconfig")
+	if err != nil {
+		panic(err)
+	}
+	defer func(dir string) {
+		os.RemoveAll(dir)
+	}(tmpdir)
+
+	home = os.Getenv("HOME")
+
+	os.Unsetenv("HOME")
+	name, err = homeDir()
+	assert.NotNil(err)
+	assert.Equal("", name)
+
+	name, err = expendHome("")
+	assert.NotNil(err)
+	assert.Equal("", name)
+
+	os.Setenv("HOME", tmpdir)
+
+	name, err = homeDir()
+	assert.Equal(tmpdir, name)
+
+	name, err = expendHome("")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = expendHome("a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "a"), name)
+
+	name, err = expendHome("~a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "~a"), name)
+
+	name, err = expendHome("~")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = expendHome("~/")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = expendHome("~/a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "a"), name)
+
+	name, err = expendHome("ab")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "ab"), name)
+
+	name, err = expendHome("/")
+	assert.Nil(err)
+	assert.Equal("/", name)
+
+	name, err = expendHome("/a")
+	assert.Nil(err)
+	assert.Equal("/a", name)
+
+	os.Setenv("HOME", home)
+}
+
+func TestAbs(t *testing.T) {
+	var (
+		home   string
+		tmpdir string
+		name   string
+		err    error
+		assert = assert.New(t)
+	)
+
+	tmpdir, err = ioutil.TempDir("", "goconfig")
+	if err != nil {
+		panic(err)
+	}
+	defer func(dir string) {
+		os.RemoveAll(dir)
+	}(tmpdir)
+
+	home = os.Getenv("HOME")
+
+	os.Unsetenv("HOME")
+	name, err = Abs("~/")
+	assert.NotNil(err)
+	assert.Equal("", name)
+
+	os.Setenv("HOME", tmpdir)
+	cwd, _ := os.Getwd()
+
+	name, err = Abs("")
+	assert.Nil(err)
+	assert.Equal(cwd, name)
+
+	name, err = Abs("a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "a"), name)
+
+	name, err = Abs("~a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "~a"), name)
+
+	name, err = Abs("~")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = Abs("~/")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = Abs("~/a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "a"), name)
+
+	name, err = Abs("ab")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "ab"), name)
+
+	name, err = Abs("/")
+	assert.Nil(err)
+	assert.Equal("/", name)
+
+	name, err = Abs("/a")
+	assert.Nil(err)
+	assert.Equal("/a", name)
+
+	os.Setenv("HOME", home)
+}
+
+func TestAbsJoin(t *testing.T) {
+	var (
+		home   string
+		tmpdir string
+		name   string
+		err    error
+		assert = assert.New(t)
+	)
+
+	tmpdir, err = ioutil.TempDir("", "goconfig")
+	if err != nil {
+		panic(err)
+	}
+	defer func(dir string) {
+		os.RemoveAll(dir)
+	}(tmpdir)
+
+	home = os.Getenv("HOME")
+	os.Setenv("HOME", tmpdir)
+
+	cwd := "/some/dir"
+
+	name, err = AbsJoin(cwd, "")
+	assert.Nil(err)
+	assert.Equal(cwd, name)
+
+	name, err = AbsJoin(cwd, "a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "a"), name)
+
+	name, err = AbsJoin(cwd, "~a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "~a"), name)
+
+	name, err = AbsJoin(cwd, "~")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = AbsJoin(cwd, "~/")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = AbsJoin(cwd, "~/a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "a"), name)
+
+	name, err = AbsJoin(cwd, "ab")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "ab"), name)
+
+	name, err = AbsJoin(cwd, "/")
+	assert.Nil(err)
+	assert.Equal("/", name)
+
+	name, err = AbsJoin(cwd, "/a")
+	assert.Nil(err)
+	assert.Equal("/a", name)
+
+	os.Setenv("HOME", home)
+}

jiangxin/gitconfig

Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

path.go
@@ -1,0 +1,75 @@
+package gitconfig
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func homeDir() (string, error) {
+	var (
+		home string
+	)
+
+	if runtime.GOOS == "windows" {
+		home = os.Getenv("USERPROFILE")
+		if home == "" {
+			home = os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		}
+	}
+	if home == "" {
+		home = os.Getenv("HOME")
+	}
+
+	if home == "" {
+		return "", fmt.Errorf("cannot find HOME")
+	}
+
+	return home, nil
+}
+
+func expendHome(name string) (string, error) {
+	if filepath.IsAbs(name) {
+		return name, nil
+	}
+
+	home, err := homeDir()
+	if err != nil {
+		return "", err
+	}
+
+	if len(name) == 0 || name == "~" {
+		return home, nil
+	} else if len(name) > 1 && name[0] == '~' && (name[1] == '/' || name[1] == '\\') {
+		return filepath.Join(home, name[2:]), nil
+	}
+
+	return filepath.Join(home, name), nil
+}
+
+// Abs returns absolute path and will expend homedir if path has "~/' prefix
+func Abs(name string) (string, error) {
+	if filepath.IsAbs(name) {
+		return name, nil
+	}
+
+	if len(name) > 0 && name[0] == '~' && (len(name) == 1 || name[1] == '/' || name[1] == '\\') {
+		return expendHome(name)
+	}
+
+	return filepath.Abs(name)
+}
+
+// AbsJoin returns absolute path, and use <dir> as parent dir for relative path
+func AbsJoin(dir, name string) (string, error) {
+	if filepath.IsAbs(name) {
+		return name, nil
+	}
+
+	if len(name) > 0 && name[0] == '~' && (len(name) == 1 || name[1] == '/' || name[1] == '\\') {
+		return expendHome(name)
+	}
+
+	return Abs(filepath.Join(dir, name))
+}

path_test.go
@@ -1,0 +1,209 @@
+package gitconfig
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExpendHome(t *testing.T) {
+	var (
+		home   string
+		tmpdir string
+		name   string
+		err    error
+		assert = assert.New(t)
+	)
+
+	tmpdir, err = ioutil.TempDir("", "gitconfig")
+	if err != nil {
+		panic(err)
+	}
+	defer func(dir string) {
+		os.RemoveAll(dir)
+	}(tmpdir)
+
+	home = os.Getenv("HOME")
+
+	os.Unsetenv("HOME")
+	name, err = homeDir()
+	assert.NotNil(err)
+	assert.Equal("", name)
+
+	name, err = expendHome("")
+	assert.NotNil(err)
+	assert.Equal("", name)
+
+	os.Setenv("HOME", tmpdir)
+
+	name, err = homeDir()
+	assert.Equal(tmpdir, name)
+
+	name, err = expendHome("")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = expendHome("a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "a"), name)
+
+	name, err = expendHome("~a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "~a"), name)
+
+	name, err = expendHome("~")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = expendHome("~/")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = expendHome("~/a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "a"), name)
+
+	name, err = expendHome("ab")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "ab"), name)
+
+	name, err = expendHome("/")
+	assert.Nil(err)
+	assert.Equal("/", name)
+
+	name, err = expendHome("/a")
+	assert.Nil(err)
+	assert.Equal("/a", name)
+
+	os.Setenv("HOME", home)
+}
+
+func TestAbs(t *testing.T) {
+	var (
+		home   string
+		tmpdir string
+		name   string
+		err    error
+		assert = assert.New(t)
+	)
+
+	tmpdir, err = ioutil.TempDir("", "gitconfig")
+	if err != nil {
+		panic(err)
+	}
+	defer func(dir string) {
+		os.RemoveAll(dir)
+	}(tmpdir)
+
+	home = os.Getenv("HOME")
+
+	os.Unsetenv("HOME")
+	name, err = Abs("~/")
+	assert.NotNil(err)
+	assert.Equal("", name)
+
+	os.Setenv("HOME", tmpdir)
+	cwd, _ := os.Getwd()
+
+	name, err = Abs("")
+	assert.Nil(err)
+	assert.Equal(cwd, name)
+
+	name, err = Abs("a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "a"), name)
+
+	name, err = Abs("~a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "~a"), name)
+
+	name, err = Abs("~")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = Abs("~/")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = Abs("~/a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "a"), name)
+
+	name, err = Abs("ab")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "ab"), name)
+
+	name, err = Abs("/")
+	assert.Nil(err)
+	assert.Equal("/", name)
+
+	name, err = Abs("/a")
+	assert.Nil(err)
+	assert.Equal("/a", name)
+
+	os.Setenv("HOME", home)
+}
+
+func TestAbsJoin(t *testing.T) {
+	var (
+		home   string
+		tmpdir string
+		name   string
+		err    error
+		assert = assert.New(t)
+	)
+
+	tmpdir, err = ioutil.TempDir("", "gitconfig")
+	if err != nil {
+		panic(err)
+	}
+	defer func(dir string) {
+		os.RemoveAll(dir)
+	}(tmpdir)
+
+	home = os.Getenv("HOME")
+	os.Setenv("HOME", tmpdir)
+
+	cwd := "/some/dir"
+
+	name, err = AbsJoin(cwd, "")
+	assert.Nil(err)
+	assert.Equal(cwd, name)
+
+	name, err = AbsJoin(cwd, "a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "a"), name)
+
+	name, err = AbsJoin(cwd, "~a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "~a"), name)
+
+	name, err = AbsJoin(cwd, "~")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = AbsJoin(cwd, "~/")
+	assert.Nil(err)
+	assert.Equal(tmpdir, name)
+
+	name, err = AbsJoin(cwd, "~/a")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(tmpdir, "a"), name)
+
+	name, err = AbsJoin(cwd, "ab")
+	assert.Nil(err)
+	assert.Equal(filepath.Join(cwd, "ab"), name)
+
+	name, err = AbsJoin(cwd, "/")
+	assert.Nil(err)
+	assert.Equal("/", name)
+
+	name, err = AbsJoin(cwd, "/a")
+	assert.Nil(err)
+	assert.Equal("/a", name)
+
+	os.Setenv("HOME", home)
+}