jiangxin/goconfig

Move cache from load to seperate file
Signed-off-by: Jiang Xin <worldhello.net@gmail.com>

cache.go
@@ -1,0 +1,33 @@
+package goconfig
+
+import (
+	"sync"
+	"time"
+)
+
+type cache struct {
+	caches map[string]cacheItem
+	mu     sync.RWMutex
+}
+
+type cacheItem struct {
+	config GitConfig
+	time   time.Time
+}
+
+var (
+	configCaches = cache{caches: make(map[string]cacheItem)}
+)
+
+func (v *cache) Set(key string, item cacheItem) {
+	v.mu.Lock()
+	v.caches[key] = item
+	v.mu.Unlock()
+}
+
+func (v *cache) Get(key string) (cacheItem, bool) {
+	v.mu.RLock()
+	item, ok := v.caches[key]
+	v.mu.RUnlock()
+	return item, ok
+}

jiangxin/gitconfig

Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

cache.go
@@ -1,0 +1,33 @@
+package gitconfig
+
+import (
+	"sync"
+	"time"
+)
+
+type cache struct {
+	caches map[string]cacheItem
+	mu     sync.RWMutex
+}
+
+type cacheItem struct {
+	config GitConfig
+	time   time.Time
+}
+
+var (
+	configCaches = cache{caches: make(map[string]cacheItem)}
+)
+
+func (v *cache) Set(key string, item cacheItem) {
+	v.mu.Lock()
+	v.caches[key] = item
+	v.mu.Unlock()
+}
+
+func (v *cache) Get(key string) (cacheItem, bool) {
+	v.mu.RLock()
+	item, ok := v.caches[key]
+	v.mu.RUnlock()
+	return item, ok
+}