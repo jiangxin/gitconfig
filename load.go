jiangxin/goconfig

Add Load() function to read git config from disk
To read a single git config for repo, use:

    Load("<path-name>", false)

To read config of repo, and with global/system config, use:

    Load("<path-name>", true)

To get system wide git config, use:

    SystemConfig()

To get user git config, use:

    UserConfig()

Signed-off-by: Jiang Xin <worldhello.net@gmail.com>

load.go
@@ -1,0 +1,169 @@
+package goconfig
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sync"
+	"time"
+)
+
+type cache struct {
+	caches map[string]cacheItem
+	mu     sync.RWMutex
+}
+
+type cacheItem struct {
+	config GitConfig
+	time   time.Time
+}
+
+var (
+	configCaches = cache{caches: make(map[string]cacheItem)}
+)
+
+func (v *cache) Set(key string, item cacheItem) {
+	v.mu.Lock()
+	v.caches[key] = item
+	v.mu.Unlock()
+}
+
+func (v *cache) Get(key string) (cacheItem, bool) {
+	v.mu.RLock()
+	item, ok := v.caches[key]
+	v.mu.RUnlock()
+	return item, ok
+}
+
+func loadOne(name string, searching bool) (GitConfig, error) {
+	var (
+		err error
+		fi  os.FileInfo
+	)
+
+	if name == "" {
+		if !searching {
+			return nil, fmt.Errorf("filepath is not provided")
+		}
+
+		name, err = os.Getwd()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// cannot find, lookup upper dir to find gitdir
+	fi, err = os.Stat(name)
+	if err != nil || fi.IsDir() {
+		if !searching {
+			return nil, nil
+		}
+
+		name, err = findGitConfig(name)
+		if err != nil || name == "" {
+			return nil, err
+		}
+
+		fi, err = os.Stat(name)
+
+		if err != nil {
+			return nil, nil
+		}
+	}
+
+	// return cache if available
+	c, ok := configCaches.Get(name)
+	if ok {
+		if fi.ModTime() == c.time {
+			return c.config, nil
+		}
+	}
+
+	buf, err := ioutil.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, _, err := Parse(buf, name)
+	if err != nil {
+		return cfg, err
+	}
+
+	// update cache
+	configCaches.Set(name, cacheItem{
+		config: cfg,
+		time:   fi.ModTime(),
+	})
+	return cfg, nil
+}
+
+func loadAll(name string, searching bool) (GitConfig, error) {
+	cfg := NewGitConfig()
+
+	sysConfig, err := SystemConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	globalConfig, err := GlobalConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	repoConfig, err := loadOne(name, searching)
+	if err != nil {
+		return nil, err
+	}
+
+	if sysConfig != nil {
+		cfg.Merge(sysConfig)
+	}
+
+	if globalConfig != nil {
+		cfg.Merge(globalConfig)
+	}
+
+	if repoConfig != nil {
+		cfg.Merge(repoConfig)
+	}
+
+	return cfg, nil
+}
+
+// LoadFile loads git config file
+func LoadFile(name string, inherit bool) (GitConfig, error) {
+	if !inherit {
+		return loadOne(name, false)
+	}
+
+	return loadAll(name, false)
+}
+
+// LoadDir loads config from gitdir
+func LoadDir(name string, inherit bool) (GitConfig, error) {
+	if !inherit {
+		return loadOne(name, true)
+	}
+
+	return loadAll(name, true)
+}
+
+// SystemConfig returns system git config, reload if necessary
+func SystemConfig() (GitConfig, error) {
+	file := systemConfigFile()
+	if file == "" {
+		return nil, nil
+	}
+
+	return loadOne(file, false)
+}
+
+// GlobalConfig returns global user config, reload if necessary
+func GlobalConfig() (GitConfig, error) {
+	file, err := globalConfigFile()
+	if err != nil {
+		return nil, err
+	}
+
+	return loadOne(file, false)
+}

path_test.go
@@ -3,6 +3,7 @@ package goconfig
 import (
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
 
@@ -204,6 +205,129 @@ func TestAbsJoin(t *testing.T) {
 	name, err = AbsJoin(cwd, "/a")
 	assert.Nil(err)
 	assert.Equal("/a", name)
+
+	os.Setenv("HOME", home)
+}
+
+func TestFindGitDir(t *testing.T) {
+	var (
+		err     error
+		dir     string
+		gitdir  string
+		workdir string
+		cfg     string
+	)
+
+	home := os.Getenv("HOME")
+
+	tmpdir, err := ioutil.TempDir("", "goconfig")
+	if err != nil {
+		panic(err)
+	}
+	defer func(dir string) {
+		os.RemoveAll(dir)
+	}(tmpdir)
+
+	os.Setenv("HOME", tmpdir)
+
+	// find in: bare.git
+	gitdir = filepath.Join(tmpdir, "bare.git")
+	cmd := exec.Command("git", "init", "--bare", gitdir, "--")
+	assert.Equal(t, nil, cmd.Run())
+	dir, err = findGitDir(gitdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(gitdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// find in: bare.git/objects/pack
+	dir, err = findGitDir(filepath.Join(gitdir, "objects", "pack"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(filepath.Join(gitdir, "objects", "pack"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// create repo2 with gitdir file repo2/.git
+	repo2 := filepath.Join(tmpdir, "repo2")
+	err = os.MkdirAll(filepath.Join(repo2, "a", "b"), 0755)
+	assert.Equal(t, nil, err)
+	err = ioutil.WriteFile(filepath.Join(repo2, ".git"),
+		[]byte("gitdir: ../bare.git"),
+		0644)
+	assert.Equal(t, nil, err)
+
+	// find in: repo2/a/b/c
+	dir, err = findGitDir(filepath.Join(repo2, "a", "b", "c"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(filepath.Join(repo2, "a", "b", "c"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// create bad gitdir file: repo2.git
+	err = ioutil.WriteFile(filepath.Join(repo2, ".git"),
+		[]byte("../bare.git"),
+		0644)
+	assert.Equal(t, nil, err)
+
+	// fail to find in repo2/a/b/c (bad gitdir file)
+	dir, err = findGitDir(filepath.Join(repo2, "a", "b", "c"))
+	assert.NotEqual(t, nil, err)
+	assert.Equal(t, "", dir)
+
+	cfg, err = findGitConfig(filepath.Join(repo2, "a", "b", "c"))
+	assert.NotEqual(t, nil, err)
+	assert.Equal(t, "", cfg)
+
+	// create worktree
+	workdir = filepath.Join(tmpdir, "workdir")
+	cmd = exec.Command("git", "init", workdir, "--")
+	assert.Equal(t, nil, cmd.Run())
+
+	gitdir = filepath.Join(workdir, ".git")
+	err = os.MkdirAll(filepath.Join(workdir, "a", "b"), 0755)
+	assert.Equal(t, nil, err)
+
+	// find in workdir
+	dir, err = findGitDir(workdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(workdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// find in workdir/.git
+	dir, err = findGitDir(gitdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(gitdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// find in workdir/.git
+	dir, err = findGitDir(filepath.Join(workdir, "a", "b", "c"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(filepath.Join(workdir, "a", "b", "c"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// fail to find in tmpdir
+	dir, err = findGitDir(tmpdir)
+	assert.Equal(t, "", dir)
+	assert.Equal(t, nil, err)
+
+	cfg, err = findGitConfig(tmpdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "", cfg)
 
 	os.Setenv("HOME", home)
 }

jiangxin/gitconfig

Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

load.go
@@ -1,0 +1,163 @@
+package gitconfig
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sync"
+	"time"
+)
+
+type cache struct {
+	caches map[string]cacheItem
+	mu     sync.RWMutex
+}
+
+type cacheItem struct {
+	config GitConfig
+	time   time.Time
+}
+
+var (
+	configCaches = cache{caches: make(map[string]cacheItem)}
+)
+
+func (v *cache) Set(key string, item cacheItem) {
+	v.mu.Lock()
+	v.caches[key] = item
+	v.mu.Unlock()
+}
+
+func (v *cache) Get(key string) (cacheItem, bool) {
+	v.mu.RLock()
+	item, ok := v.caches[key]
+	v.mu.RUnlock()
+	return item, ok
+}
+
+func loadOne(name string, searching bool) (GitConfig, error) {
+	var (
+		err error
+		fi  os.FileInfo
+	)
+
+	if name == "" {
+		if !searching {
+			return nil, fmt.Errorf("filepath is not provided")
+		}
+
+		name, err = os.Getwd()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// cannot find, lookup upper dir to find gitdir
+	fi, err = os.Stat(name)
+	if err != nil || fi.IsDir() {
+		if !searching {
+			return nil, nil
+		}
+
+		name, err = findGitConfig(name)
+		if err != nil || name == "" {
+			return nil, err
+		}
+	}
+
+	// return cache if available
+	c, ok := configCaches.Get(name)
+	if ok {
+		if fi.ModTime() == c.time {
+			return c.config, nil
+		}
+	}
+
+	buf, err := ioutil.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, _, err := Parse(buf, name)
+	if err != nil {
+		return cfg, err
+	}
+
+	// update cache
+	configCaches.Set(name, cacheItem{
+		config: cfg,
+		time:   fi.ModTime(),
+	})
+	return cfg, nil
+}
+
+func loadAll(name string, searching bool) (GitConfig, error) {
+	cfg := NewGitConfig()
+
+	sysConfig, err := SystemConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	globalConfig, err := GlobalConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	repoConfig, err := loadOne(name, searching)
+	if err != nil {
+		return nil, err
+	}
+
+	if sysConfig != nil {
+		cfg.Merge(sysConfig)
+	}
+
+	if globalConfig != nil {
+		cfg.Merge(globalConfig)
+	}
+
+	if repoConfig != nil {
+		cfg.Merge(repoConfig)
+	}
+
+	return cfg, nil
+}
+
+// LoadFile loads git config file
+func LoadFile(name string, inherit bool) (GitConfig, error) {
+	if !inherit {
+		return loadOne(name, false)
+	}
+
+	return loadAll(name, false)
+}
+
+// LoadDir loads config from gitdir
+func LoadDir(name string, inherit bool) (GitConfig, error) {
+	if !inherit {
+		return loadOne(name, true)
+	}
+
+	return loadAll(name, true)
+}
+
+// SystemConfig returns system git config, reload if necessary
+func SystemConfig() (GitConfig, error) {
+	file := systemConfigFile()
+	if file == "" {
+		return nil, nil
+	}
+
+	return loadOne(file, false)
+}
+
+// GlobalConfig returns global user config, reload if necessary
+func GlobalConfig() (GitConfig, error) {
+	file, err := globalConfigFile()
+	if err != nil {
+		return nil, err
+	}
+
+	return loadOne(file, false)
+}

path_test.go
@@ -3,6 +3,7 @@ package gitconfig
 import (
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
 
@@ -204,6 +205,129 @@ func TestAbsJoin(t *testing.T) {
 	name, err = AbsJoin(cwd, "/a")
 	assert.Nil(err)
 	assert.Equal("/a", name)
+
+	os.Setenv("HOME", home)
+}
+
+func TestFindGitDir(t *testing.T) {
+	var (
+		err     error
+		dir     string
+		gitdir  string
+		workdir string
+		cfg     string
+	)
+
+	home := os.Getenv("HOME")
+
+	tmpdir, err := ioutil.TempDir("", "gitconfig")
+	if err != nil {
+		panic(err)
+	}
+	defer func(dir string) {
+		os.RemoveAll(dir)
+	}(tmpdir)
+
+	os.Setenv("HOME", tmpdir)
+
+	// find in: bare.git
+	gitdir = filepath.Join(tmpdir, "bare.git")
+	cmd := exec.Command("git", "init", "--bare", gitdir, "--")
+	assert.Equal(t, nil, cmd.Run())
+	dir, err = findGitDir(gitdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(gitdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// find in: bare.git/objects/pack
+	dir, err = findGitDir(filepath.Join(gitdir, "objects", "pack"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(filepath.Join(gitdir, "objects", "pack"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// create repo2 with gitdir file repo2/.git
+	repo2 := filepath.Join(tmpdir, "repo2")
+	err = os.MkdirAll(filepath.Join(repo2, "a", "b"), 0755)
+	assert.Equal(t, nil, err)
+	err = ioutil.WriteFile(filepath.Join(repo2, ".git"),
+		[]byte("gitdir: ../bare.git"),
+		0644)
+	assert.Equal(t, nil, err)
+
+	// find in: repo2/a/b/c
+	dir, err = findGitDir(filepath.Join(repo2, "a", "b", "c"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(filepath.Join(repo2, "a", "b", "c"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// create bad gitdir file: repo2.git
+	err = ioutil.WriteFile(filepath.Join(repo2, ".git"),
+		[]byte("../bare.git"),
+		0644)
+	assert.Equal(t, nil, err)
+
+	// fail to find in repo2/a/b/c (bad gitdir file)
+	dir, err = findGitDir(filepath.Join(repo2, "a", "b", "c"))
+	assert.NotEqual(t, nil, err)
+	assert.Equal(t, "", dir)
+
+	cfg, err = findGitConfig(filepath.Join(repo2, "a", "b", "c"))
+	assert.NotEqual(t, nil, err)
+	assert.Equal(t, "", cfg)
+
+	// create worktree
+	workdir = filepath.Join(tmpdir, "workdir")
+	cmd = exec.Command("git", "init", workdir, "--")
+	assert.Equal(t, nil, cmd.Run())
+
+	gitdir = filepath.Join(workdir, ".git")
+	err = os.MkdirAll(filepath.Join(workdir, "a", "b"), 0755)
+	assert.Equal(t, nil, err)
+
+	// find in workdir
+	dir, err = findGitDir(workdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(workdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// find in workdir/.git
+	dir, err = findGitDir(gitdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(gitdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// find in workdir/.git
+	dir, err = findGitDir(filepath.Join(workdir, "a", "b", "c"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, gitdir, dir)
+
+	cfg, err = findGitConfig(filepath.Join(workdir, "a", "b", "c"))
+	assert.Equal(t, nil, err)
+	assert.Equal(t, filepath.Join(gitdir, "config"), cfg)
+
+	// fail to find in tmpdir
+	dir, err = findGitDir(tmpdir)
+	assert.Equal(t, "", dir)
+	assert.Equal(t, nil, err)
+
+	cfg, err = findGitConfig(tmpdir)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "", cfg)
 
 	os.Setenv("HOME", home)
 }